f9f9299c226ba5e0b82629d79884b6b0da5b813e
Delan Azabani <delan@azabani.com>
2014-05-23 00:51:46 +0800

    include list.h in assignment.h

--- assignment.h
+++ assignment.h
@@ -4,6 +4,7 @@
 #include <cnet.h>
 
 #include "type.h"
+#include "list.h"
 
 #define CC200_NODES 5
 
@@ -12,6 +13,8 @@ extern int cc200_routing_table[CC200_NODES][CC200_NODES];
 extern cc200_byte cc200_next_seq_to_send[CC200_NODES];
 extern cc200_byte cc200_next_data_seq_expected[CC200_NODES];
 extern cc200_byte cc200_next_ack_seq_expected[CC200_NODES];
+
+extern cc200_list cc200_frame_queue[CC200_NODES];
 
 EVENT_HANDLER(cc200_test);
 EVENT_HANDLER(cc200_reboot_node);
